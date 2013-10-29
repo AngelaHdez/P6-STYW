17b18abc49f4ff4d70698855fca74b5f20780a13
Angela <angelahdezd@gmail.com>
2013-10-29 17:14:48 +0000

    arreglando test

--- spec/RockPaperScissors/app_spec.rb
+++ spec/RockPaperScissors/app_spec.rb
@@ -6,97 +6,66 @@ describe RockPaperScissors::App do
 	def app
         Rack::MockRequest.new(Rack::Session::Cookie.new(RockPaperScissors::App.new, 
         	:secret =>'cookie')) 
-
   	end
+
 
 	context "/" do
 		it "debe devolver el codigo 200 success" do
-			res =app.get('/')
+			res = app.get("/")
 			res.status.should == 200
-			#pp app
+			
 			
 		end
 
 		it "debe mostrar RPS" do
-			app.get('/').header == 'RPS'
+			app.post('/').should match('RPS')
 		end
 	end	
 
-	context "/?choice='scissors'" do 
+	context "/?choice=scissors" do 
 		it "debe devolver el codigo 200" do
-			#puts "**************************#{rack_mock_session.cookie_jar.inspect}" 
-			app.get("/?choice='scissors'").status.should == 200
-
+			app.get("/?choice=scissors").status.should == 200
 		end
 
 		it "debe mostrar tijeras" do 
-			app.get("/?choice='scissors'").body.include?("http://banot.etsii.ull.es/alu4079/STYW/rps/tijeras.png")
+			app.get("/?choice=scissors").body.should match("http://banot.etsii.ull.es/alu4079/STYW/rps/tijeras.png")
 		end
 
-		it "debe ganar" do
-			computer_throw='paper'
-			app.get("/?choice='scissors'").body.include?("Ganaste!")
-		end
+		it "resultado" do
+			app.get("/?choice=scissors").body.include?('<h2>Ganaste!</h2>')
 
-		it "debe perder" do
-			computer_throw='rock'
-			app.get("/?choice='scissors'").body.include?("Perdiste!")
-		end
-
-		it "empate" do
-			computer_throw = 'scissors'
-			app.get("/?choice='scissors'").body.include?("Empate!")
 		end
 	end
 
-	context "/?choice='rock'" do 
+	context "/?choice=scissors" do 
+		it "debe tener boton jugar " do
+			app.post("/?choice=scissors").body.should match("<a href='/'>")
+			app.post("/?choice=scissors").body.should match("Juega otra vez")
+		end
+	end
+
+	
+
+	context "/?choice=rock" do 
 		it "debe devolver el codigo 200" do
-			app.get("/?choice='rock'").status.should == 200
+			app.get("/?choice=rock").status.should == 200
 		end
 
 		it "debe mostrar roca" do 
-			app.get("/?choice='rock'").body.include?("http://banot.etsii.ull.es/alu4079/STYW/rps/piedra.png")
+			app.get("/?choice=rock").body.should match("http://banot.etsii.ull.es/alu4079/STYW/rps/piedra.png")
 		end
 
-		it "debe ganar" do
-			computer_throw='scissors'
-			app.get("/?choice='rock'").body.include?("Ganaste!")
-		end
-
-		it "debe perder" do
-			computer_throw='paper'
-			app.get("/?choice='rock'").body.include?("Perdiste!")
-		end
-
-		it "empate" do
-			computer_throw = 'rock'
-			app.get("/?choice='rock'").body.include?("Empate!")
-		end
 	end
 
-	context "/?choice='paper'" do 
+	context "/?choice=paper" do 
 		it "debe devolver el codigo 200" do
-			app.get("/?choice='paper'").status.should == 200
+			app.get("/?choice=paper").status.should == 200
 		end
 
 		it "debe mostrar papel" do 
-			app.get("/?choice='paper'").body.include?("http://banot.etsii.ull.es/alu4079/STYW/rps/papel.png")
+			app.get("/?choice=paper").body.should match("http://banot.etsii.ull.es/alu4079/STYW/rps/papel.png")
 		end
 
-		it "debe ganar" do
-			computer_throw='rock'
-			app.get("/?choice='paper'").body.include?("Ganaste!")
-		end
-
-		it "debe perder" do
-			computer_throw='scissors'
-			app.get("/?choice='paper'").body.include?("Perdiste!")
-		end
-
-		it "empate" do
-			computer_throw = 'paper'
-			app.get("/?choice='paper'").body.include?("Empate!")
-		end
 	end
 
 	##cookie
